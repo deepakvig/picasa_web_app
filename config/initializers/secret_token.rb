94b12e59b9fe014ead91a75acc9cfe0eb1f082b4
Deepak Vig <deepakvig@gmail.com>
2013-03-26 17:01:37 +0530

    Base App with Google Sign In

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PicasaWebApp::Application.config.secret_token = '8e796b8b77a70df6314758fb2b929a1902df5dcc36efc3e4b98971af7426c10ee3fa6aa9b0a077d576bd31a2b7ffe7a35db047fa7aec499ff8bcd62e448695a9'
